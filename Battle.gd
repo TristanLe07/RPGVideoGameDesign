cd29d1046093e2159b2ae89ab2358d4a84abd489
SiyprYT <68062506+SiyprYT@users.noreply.github.com>
2022-07-27 08:03:02 +0800

    game hehe

--- Battle.gd
+++ Battle.gd
@@ -1,6 +1,35 @@
 extends Node
 
 onready var enemy = $Enemy
+onready var playerStats = $PlayerStats
+onready var buttons = $Ui/GridContainer
 
-func _on_Button_pressed():
-	enemy.hp -= 4
+func _ready():
+	start_player_turn()
+
+func start_enemy_turn():
+	buttons.hide()
+	if enemy != null:
+		enemy.attack(playerStats)
+		yield(enemy, "end_turn")
+	start_player_turn()
+	
+func start_player_turn():
+	buttons.show()
+	playerStats.ap = playerStats.max_ap
+	yield(playerStats, "end_turn")
+	start_enemy_turn()
+
+func _on_Attack_pressed():
+	if enemy != null:
+		enemy.take_damage(4)
+		playerStats.ap -=1
+
+
+func _on_Enemy_enemy_died():
+	buttons.hide()
+	enemy = null
+
+
+func _on_Heal_pressed():
+	pass # Replace with function body.
--- Enemy.gd
+++ Enemy.gd
@@ -2,8 +2,31 @@ extends Node2D
 
 var hp = 25 setget set_hp
 
+onready var timer = $Timer
 onready var hpLabel = $hpLabel
+onready var animationPlayer = $AnimationPlayer
+
+signal enemy_died
+signal end_turn
 
 func set_hp(new_hp):
 	hp = new_hp
 	hpLabel.text = str(hp) + "hp"
+
+func attack(target) -> void:
+	yield(get_tree().create_timer(0.4), "timeout")
+	animationPlayer.play("Attack")
+	yield(animationPlayer, "animation_finished")
+	target.hp -= 7
+	emit_signal("end_turn")
+
+func take_damage(amount):
+	self.hp -= amount
+	if is_dead():
+		emit_signal("enemy_died")
+		queue_free()
+	else:
+		animationPlayer.play("Shake")
+	
+func is_dead():
+	return hp <= 0
