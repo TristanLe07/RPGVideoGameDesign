eceb09e50c3d650a8f46ae1a0b8c0676ca25e15e
TristanLe07 <107384818+TristanLe07@users.noreply.github.com>
2022-07-27 09:42:30 +0800

    changed

--- Battle.gd
+++ Battle.gd
@@ -2,32 +2,27 @@ extends Node
 
 onready var enemy = $Enemy
 onready var playerStats = $PlayerStats
-onready var buttons = $Ui/GridContainer
+onready var battleActionButtons = $Ui/GridContainer
 
 func _ready():
 	start_player_turn()
 
 func start_enemy_turn():
-	buttons.hide()
+	battleActionButtons.hide()
 	if enemy != null:
 		enemy.attack(playerStats)
 		yield(enemy, "end_turn")
 	start_player_turn()
 	
 func start_player_turn():
-	buttons.show()
+	battleActionButtons.show()
 	playerStats.ap = playerStats.max_ap
 	yield(playerStats, "end_turn")
 	start_enemy_turn()
 
-func _on_Attack_pressed():
-	if enemy != null:
-		enemy.take_damage(4)
-		playerStats.ap -=1
-
 
 func _on_Enemy_enemy_died():
-	buttons.hide()
+	battleActionButtons.hide()
 	enemy = null
 
 
--- Enemy.gd
+++ Enemy.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var hp = 25 setget set_hp
+var target = null
 
 onready var timer = $Timer
 onready var hpLabel = $hpLabel
@@ -16,9 +17,13 @@ func set_hp(new_hp):
 func attack(target) -> void:
 	yield(get_tree().create_timer(0.4), "timeout")
 	animationPlayer.play("Attack")
+	self.target = target
 	yield(animationPlayer, "animation_finished")
-	target.hp -= 7
+	self.target = null
 	emit_signal("end_turn")
+
+func deal_damage():
+	print("dealt damage")
 
 func take_damage(amount):
 	self.hp -= amount
