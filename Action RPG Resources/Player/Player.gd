220af33ce2f215101290e848778b2de42808a61f
SiyprYT <68062506+SiyprYT@users.noreply.github.com>
2022-08-01 22:09:30 +0800

    updated a lot

--- Action RPG Resources/Player/Player.gd
+++ Action RPG Resources/Player/Player.gd
@@ -4,13 +4,27 @@ const ACCELERATION = 500
 const MAX_SPEED = 80
 const FRICTION = 500
 
+var state = MOVE
 var velocity = Vector2.ZERO
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
+enum {
+	MOVE,
+	ROLL
+}
+
+
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+	
+	
+	
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
