b482925ec5812bf8e7a86d716802eff4ec2d5142
SiyprYT <68062506+SiyprYT@users.noreply.github.com>
2022-08-27 00:51:10 +0800

    Updates!
    - Discord Rich Presence has been ADDED!
    - Cleaned up project files a little bit
    - Removed unneeded files
    - Fixing House Collisions

---
+++ Action RPG Resources/DiscordRPC/World/PokeCenter.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+func _ready():
+	update_activity()
+	
+func update_activity() -> void:
+	var activity = Discord.Activity.new()
+	activity.set_type(Discord.ActivityType.Playing)
+	activity.set_state("Playing Pokémon")
+	activity.set_details("Inside the PokéCenter!")
+
+	var assets = activity.get_assets()
+	assets.set_large_image("logo")
+	assets.set_large_text("Pokémon: The Game")
+	assets.set_small_image("logo")
+	assets.set_small_text("Pokémon: The Game")
+
+	var result = yield(Discord.activity_manager.update_activity(activity), "result").result
+	if result != Discord.Result.Ok:
+		push_error(result)
