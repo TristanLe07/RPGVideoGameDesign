cdc9b49e2a3ac7d57a0ae531b59603a24fb2f3a4
TristanLe07 <107384818+TristanLe07@users.noreply.github.com>
2022-10-17 13:34:00 +0800

    networking test

--- Networking/Game.gd
+++ Networking/Game.gd
@@ -14,3 +14,9 @@ func _ready():
 func create_players():
 	for id in Net.peer_ids:
 		create_player(id)
+	create_player(Net.net_id)
+	
+func create_player(id):
+	var p = preload("res://Action RPG Resources/Player/Player.tscn").instance()
+	add_child(p)
+	p.initialized(id)
