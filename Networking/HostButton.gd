ce01dc91eb17768988198220d21056709f7202eb
TristanLe07 <107384818+TristanLe07@users.noreply.github.com>
2022-10-17 13:48:42 +0800

    network test fix

--- Networking/HostButton.gd
+++ Networking/HostButton.gd
@@ -3,7 +3,7 @@ extends Button
 signal set_connect_type
 
 func _ready():
-	$IP.text = "IP: " + str(IP.get_local_addresses()[7])
+	$IP.text = "IP: " + str(IP.get_local_addresses())
 
 
 func _on_HostButton_pressed():
