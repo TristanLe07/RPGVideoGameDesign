edcf6193d8a592ea67239cd4cf5884cd4d0a8a85
TristanLe07 <107384818+TristanLe07@users.noreply.github.com>
2022-10-24 13:31:50 +0800

    massive updates
    lots of things, movement, sprites, maps, gui everything is getting changed!

--- Action RPG Resources/World/Day_Night_Cycle.gd
+++ Action RPG Resources/World/Day_Night_Cycle.gd
@@ -2,6 +2,7 @@ extends CanvasModulate
 
 func _process(_delta):
 	var time = OS.get_time()
+
 	var TimeInSeconds = time.hour * 3600 + time.minute * 60 + time.second
 	var CurrentFrame = range_lerp(TimeInSeconds,0,86400,0,24)
 	$AnimationPlayer.play("Day_Night_Cycle")
